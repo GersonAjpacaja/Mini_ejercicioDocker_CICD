0fe03ae6cf22005bc0eedd09a92907dda25cce12
ChaconFrancoLuis <francofer7775@gmail.com>
2021-05-14 02:00:14 +0000

    aplicacion para ejercicio docker

---
+++ app.py
@@ -0,0 +1,15 @@
+import flask
+from flask import request
+from flask import render_template
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5051)
