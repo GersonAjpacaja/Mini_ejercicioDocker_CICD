e20fb1c6bc83ba7c9dbc1c89e00dbdae9405a81e
GersonAjpacaja <gersonponcio@gmail.com>
2021-05-20 01:42:11 +0000

    Se agrego archivo .bash

--- app.py
+++ app.py
@@ -8,6 +8,5 @@ app = flask.Flask(__name__)
 def main():
     return render_template("index.html")
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5051)
---
+++ app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir tempodir
+mkdir tempodir/templates
+mkdir tempodir/static
+
+cp app.py tempodir/.
+cp -r templates/* tempodir/templates/.
+cp -r static/* tempodir/static/.
+
+echo "FROM python" >> tempodir/Dockerfile
+echo "RUN pip install flask" >> tempodir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempodir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempodir/Dockerfile
+echo "COPY app.py /home/myapp/" >> tempodir/Dockerfile
+echo "EXPOSE 5051" >> tempodir/
+echo "CMD python /home/myapp/app.py" >> tempodir/Dockerfile
+
+cd tempodir
+docker build -t appimagen .
+docker run -t -d -p 5051:5051 --name appejercicio appimagen
+docker ps -a 
